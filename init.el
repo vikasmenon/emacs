4f4616f22563db9dc263730efea9a19bc1022f6a
Vikas Menon <menonvikas@gmail.com>
2011-04-26 10:32:04 -0400

    adding redspace and full screen modes

--- init.el
+++ init.el
@@ -49,6 +49,17 @@
 (global-set-key (kbd "C-x r C-w")   'rm-kill-region)
 (global-set-key (kbd "C-x r M-w")   'rm-kill-ring-save)
 
+(defun toggle-fullscreen (&optional f)
+  (interactive)
+  (let ((current-value (frame-parameter nil 'fullscreen)))
+      (set-frame-parameter nil 'fullscreen
+         (if (equal 'fullboth current-value)
+             (if (boundp 'old-fullscreen) old-fullscreen nil)
+                 (progn (setq old-fullscreen current-value)
+                        'fullboth)))))
+(global-set-key [f11] 'toggle-fullscreen)
+
+
 
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
