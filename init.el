23adf7512d308e53d436cdd83a23c0cd75a96317
Vikas Menon <menonvikas@gmail.com>
2011-05-20 16:00:31 -0400

    adding cedet

--- init.el
+++ init.el
@@ -78,3 +78,6 @@
 (load-library "init_haskell.el")
 
 (load-file "~/.emacs.d/cedet/common/cedet.el")
+(global-ede-mode 1)                      ; Enable the Project management system
+(semantic-load-enable-code-helpers)      ; Enable prototype help and smart completion 
+(global-srecode-minor-mode 1)            ; Enable template insertion menu
