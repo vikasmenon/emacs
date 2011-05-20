39186bd7c0cdd3a81e3155c24af7ead77fd1a38d
Vikas Menon <menonvikas@gmail.com>
2011-05-20 16:25:46 -0400

    working ecb browser

--- init.el
+++ init.el
@@ -77,7 +77,11 @@
 
 (load-library "init_haskell.el")
 
-(load-file "~/.emacs.d/cedet/common/cedet.el")
+(load-file "~/.emacs.d/cedet-1.0pre6/common/cedet.el")
 (global-ede-mode 1)                      ; Enable the Project management system
-(semantic-load-enable-code-helpers)      ; Enable prototype help and smart completion 
+(semantic-load-enable-code-helpers)      ; Enable prototype help and smart completion
 (global-srecode-minor-mode 1)            ; Enable template insertion menu
+
+
+(add-to-list 'load-path "~/.emacs.d/ecb-2.40/")
+(require 'ecb)
