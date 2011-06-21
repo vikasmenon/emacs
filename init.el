2926c723fa9820053e4b7fc1f4905609c39ceb08
Vikas Menon <menonvikas@gmail.com>
2011-06-21 16:07:03 -0400

    adding mobile org settings

--- init.el
+++ init.el
@@ -126,3 +126,10 @@
 
 ;; Copy from clipboard
 (setq x-select-enable-clipboard t)
+
+;; Set to the location of your Org files on your local system
+(setq org-directory "~/org")
+;; Set to the name of the file where new notes will be stored
+(setq org-mobile-inbox-for-pull "~/org/flagged.org")
+;; Set to <your Dropbox root directory>/MobileOrg.
+(setq org-mobile-directory "~/Dropbox/MobileOrg")
